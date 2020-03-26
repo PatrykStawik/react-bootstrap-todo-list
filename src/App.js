bb7e43c45004aabae0bf12210e5f316d7f0aa845
Patryk Stawik <patrykstawik@gmail.com>
2020-03-26 21:06:45 +0100

    coomit 3

--- src/App.js
+++ src/App.js
@@ -9,17 +9,30 @@ import TodoList from './components/TodoList'
 
 class App extends Component {
   state = { 
-    items: [{id: 1, title: 'wake up'},{id: 2, title: 'eat breakfast'}],
+    items: [],
     id: uuidv4(),
     item: '',
     editItem: false,
    }
 
    handleChange=(e)=>{
-
+    this.setState({
+      item: e.target.value
+    })
    }
    handleSubmit=(e)=>{
-    
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    }
+    const updatedItems = [...this.state.items, newItem]
+    this.setState({
+      items: updatedItems,
+      item: '',
+      id: uuidv4(),
+      editItem: false
+    })
   }
   clearList=()=>{
     
@@ -32,16 +45,19 @@ class App extends Component {
   }
 
   render() { 
+    
     return ( 
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-5">
             <h3 className="text-capitalize text-center">
             
-              <TodoList item={this.state.item} 
-              handleChange={this.handleChange} 
-              handleSubmit={this.handleSubmit} 
-              editItem={this.state.editItem}/>
+              <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
               <TodoInput items={this.state.items} 
               clearList={this.clearList} 
               handleDelete={this.handleDelete} 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
-//12345
+import Item from "./TodoItem";
 
 class TodoList extends Component {
-    state = {  }
+    
     render() { 
+        const {items, clearList, handleDelete, handleEdit}=this.props;
         return ( 
-            <>
-            <div>todo list</div>
-            <TodoItem/>
-            </>
+            <ul className="list-group my-5">
+                <h3 className="text-capitalize text-center">todo list</h3>
+                {items.map(item => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            />
+          );
+        })}
+                <button type="button" className="btn btn-danger btn-block text-uppercase mt-5" onClick={clearList}>clear list</button>
+            </ul>
          );
     }
 }
